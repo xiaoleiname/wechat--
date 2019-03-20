ea961f9ed5e4d9a3d2797d3f1cdca484c3b765ae
xiaolei <1653349993qq.com>
2019-03-20 11:26:12 +0800

    '第2次提交’

--- body/whdiqlhf.js
+++ body/whdiqlhf.js
@@ -1,3 +1,17 @@
 /**
  * Created by Administrator on 2019/3/20.
  */
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) =>{
+
+})
+
+
+
+app.listen(3000,err =>{
+    if(!err) console.log('服务器启动成功了~');
+    else console.log(err);
+})
